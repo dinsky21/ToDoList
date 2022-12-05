2f51b28fb4ebf5142c42c08d679ee8693511f321
Dinsky21 <dinsky21@gmail.com>
2022-12-05 14:36:26 +0800

    feat: add logout feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -34,4 +34,13 @@ router.post('/register', (req, res) => {
   })
 })
 
+router.get('/logout', (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err)
+    }
+    res.redirect('/users/login')
+  })
+})
+
 module.exports = router
